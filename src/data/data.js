d8642f38079328f395ede6148bcb95bbad3922d5
developer24Sri <88076621+developer24Sri@users.noreply.github.com>
2024-08-05 17:58:12 +0530

    updated data of the project

--- src/data/data.js
+++ src/data/data.js
@@ -1,75 +1,83 @@
-import imageProduct1 from '../assets/images/1.png' 
-import imageProduct2 from '../assets/images/2.png' 
-import imageProduct3 from '../assets/images/3.png' 
-import imageProduct4 from '../assets/images/4.png' 
-import imageProduct5 from '../assets/images/5.png' 
-import imageProduct6 from '../assets/images/6.png' 
-import imageProduct7 from '../assets/images/7.png' 
-import imageProduct8 from '../assets/images/8.png' 
+import imageProduct1 from "../assets/images/1.png";
+import imageProduct2 from "../assets/images/2.png";
+import imageProduct3 from "../assets/images/3.png";
+import imageProduct4 from "../assets/images/4.png";
+import imageProduct5 from "../assets/images/5.png";
+import imageProduct6 from "../assets/images/6.png";
+import imageProduct7 from "../assets/images/7.png";
+import imageProduct8 from "../assets/images/8.png";
 
 export const products = [
-    {
-        id: 1,
-        name: 'Tomatoes',
-        price: 30,
-        image: imageProduct1,
-        description: 'Fresh and juicy, our vine-ripened tomatoes are bursting with flavor, perfect for salads, sauces, and sandwiches. Grown organically, they offer a taste of summer with every bite.',
-        slug: 'Tomatoes'
-    },
-    {
-        id: 2,
-        name: 'Onions',
-        price: 45,
-        image: imageProduct2,
-        description: 'Versatile and flavorful, our farm-fresh onions add the perfect zest to any dish, from soups to salads. Hand-picked for quality, they bring a delicious crunch and rich taste to your kitchen',
-        slug: 'Onions'
-    },
-    {
-        id: 3,
-        name: 'Chillies',
-        price: 25,
-        image: imageProduct3,
-        description: 'Spice up your meals with our vibrant and fiery chillies, perfect for adding a kick to any dish. Freshly harvested and full of flavor, they are an essential ingredient for culinary enthusiasts.',
-        slug: 'Chillies'
-    },
-    {
-        id: 4,
-        name: '20',
-        price: 201,
-        image: imageProduct4,
-        description: 'Discover the tropical goodness of our premium coconuts, packed with refreshing water and rich, creamy flesh. Perfect for hydration, cooking, and desserts, these versatile coconuts bring a taste of paradise to your home.',
-        slug: 'Coconuts'
-    },
-    {
-        id: 5,
-        name: '10',
-        price: 251,
-        image: imageProduct5,
-        description: 'Brighten your dishes with our zesty, sun-ripened lemons, perfect for adding a burst of fresh flavor. Rich in vitamin C and full of tangy goodness, these lemons are ideal for cooking, baking, and refreshing beverages.',
-        slug: 'Lemons'
-    },
-    {
-        id: 6,
-        name: '35',
-        price: 111,
-        image: imageProduct6,
-        description: 'Add vibrancy to your cooking with our crisp and colorful capsicums, bursting with sweet and tangy flavors. Versatile for salads, grilling, and stuffing, these fresh bell peppers elevate every meal with their delightful crunch.',
-        slug: 'Capsicums'
-    },
-    {
-        id: 7,
-        name: '100',
-        price: 121,
-        image: imageProduct7,
-        description: '"Indulge in our premium cauliflowers, known for their crisp texture and mild, nutty flavor. Perfect for roasting, steaming, or as a nutritious side dish, these versatile vegetables promise wholesome goodness with every bite.',
-        slug: 'Cauliflowers'
-    },
-    {
-        id: 8,
-        name: '80',
-        price: 220,
-        image: imageProduct8,
-        description: 'Explore the earthy richness of our gourmet mushrooms, hand-picked for their exquisite taste and texture. Whether sautéed, grilled, or added to sauces, these mushrooms elevate any dish with their savory essence.',
-        slug: 'Mushrooms'
-    },
+  {
+    id: 1,
+    name: "Tomatoes",
+    price: 30,
+    image: imageProduct1,
+    description:
+      "Fresh and juicy, our vine-ripened tomatoes are bursting with flavor, perfect for salads, sauces, and sandwiches. Grown organically, they offer a taste of summer with every bite.",
+    slug: "Tomatoes",
+  },
+  {
+    id: 2,
+    name: "Onions",
+    price: 45,
+    image: imageProduct2,
+    description:
+      "Versatile and flavorful, our farm-fresh onions add the perfect zest to any dish, from soups to salads. Hand-picked for quality, they bring a delicious crunch and rich taste to your kitchen",
+    slug: "Onions",
+  },
+  {
+    id: 3,
+    name: "Chillies",
+    price: 25,
+    image: imageProduct3,
+    description:
+      "Spice up your meals with our vibrant and fiery chillies, perfect for adding a kick to any dish. Freshly harvested and full of flavor, they are an essential ingredient for culinary enthusiasts.",
+    slug: "Chillies",
+  },
+  {
+    id: 4,
+    name: "Coconuts",
+    price: 201,
+    image: imageProduct4,
+    description:
+      "Discover the tropical goodness of our premium coconuts, packed with refreshing water and rich, creamy flesh. Perfect for hydration, cooking, and desserts, these versatile coconuts bring a taste of paradise to your home.",
+    slug: "Coconuts",
+  },
+  {
+    id: 5,
+    name: "Lemons",
+    price: 251,
+    image: imageProduct5,
+    description:
+      "Brighten your dishes with our zesty, sun-ripened lemons, perfect for adding a burst of fresh flavor. Rich in vitamin C and full of tangy goodness, these lemons are ideal for cooking, baking, and refreshing beverages.",
+    slug: "Lemons",
+  },
+  {
+    id: 6,
+    name: "Capsicums",
+    price: 111,
+    image: imageProduct6,
+    description:
+      "Add vibrancy to your cooking with our crisp and colorful capsicums, bursting with sweet and tangy flavors. Versatile for salads, grilling, and stuffing, these fresh bell peppers elevate every meal with their delightful crunch.",
+    slug: "Capsicums",
+  },
+  {
+    id: 7,
+    name: "Cauliflower",
+    price: 121,
+    image: imageProduct7,
+    description:
+      '"Indulge in our premium cauliflowers, known for their crisp texture and mild, nutty flavor. Perfect for roasting, steaming, or as a nutritious side dish, these versatile vegetables promise wholesome goodness with every bite.',
+    slug: "Cauliflowers",
+  },
+  {
+    id: 8,
+    name: "Mushroom",
+    price: 220,
+    image: imageProduct8,
+    description:
+      "Explore the earthy richness of our gourmet mushrooms, hand-picked for their exquisite taste and texture. Whether sautéed, grilled, or added to sauces, these mushrooms elevate any dish with their savory essence.",
+    slug: "Mushrooms",
+  },
 ];
